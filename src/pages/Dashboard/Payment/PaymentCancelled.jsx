b106489d4be61b20ccb691f63b5d36a15fadd085
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-12-08 23:52:45 +0600

    Payment updated success history

--- src/pages/Dashboard/Payment/PaymentCancelled.jsx
+++ src/pages/Dashboard/Payment/PaymentCancelled.jsx
@@ -14,21 +14,3 @@ const PaymentCancelled = () => {
 };
 
 export default PaymentCancelled;
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router';
-
-// const PaymentCancelled = () => {
-//     return (
-//         <div>
-//             <h2>Payment is cancelled. Please try again</h2>
-//             <Link to="/dashboard/my-parcels">
-//             <button className='btn btn-primary text-black'>Try Again</button></Link>
-//         </div>
-//     );
-// };
-
-// export default PaymentCancelled;
