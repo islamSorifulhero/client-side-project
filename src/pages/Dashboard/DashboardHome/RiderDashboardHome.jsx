a5355ea213343a9019186103e2535a80832b7db4
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-12-09 23:39:22 +0600

    Some code updated

--- src/pages/Dashboard/DashboardHome/RiderDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/RiderDashboardHome.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const RiderDashboardHome = () => {
-    return (
-        <div>
-            <h2 className='text-4xl'>Rider Dashing</h2>
-        </div>
-    );
-};
+// const RiderDashboardHome = () => {
+//     return (
+//         <div>
+//             <h2 className='text-4xl'>Rider Dashing</h2>
+//         </div>
+//     );
+// };
 
-export default RiderDashboardHome;
+// export default RiderDashboardHome;
