2de8ff159e00ada92dc7c77953d4e73a6e289e78
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-12-10 23:46:17 +0600

    some code updated and router code update

--- src/pages/Dashboard/DashboardHome/UserDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/UserDashboardHome.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const UserDashboardHome = () => {
-    return (
-        <div>
-            <h2 className='text-4xl'>User Dashing</h2>
-        </div>
-    );
-};
+// const UserDashboardHome = () => {
+//     return (
+//         <div>
+//             <h2 className='text-4xl'>User Dashing</h2>
+//         </div>
+//     );
+// };
 
-export default UserDashboardHome;
+// export default UserDashboardHome;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
-// import Coverage from "../pages/Coverage/Coverage";
 import AuthLayout from "../layouts/AuthLayout";
 import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Login/Register/Register";
@@ -11,13 +10,13 @@ import Payment from "../pages/Dashboard/Payment/Payment";
 import PaymentSuccess from "../pages/Dashboard/Payment/PaymentSuccess";
 import PaymentCancelled from "../pages/Dashboard/Payment/PaymentCancelled";
 import PaymentHistory from "../pages/Dashboard/Payment/PaymentHistory";
-import ApproveRiders from "../pages/Dashboard/ApproveRiders/ApproveRiders";
-import UsersManagement from "../pages/Dashboard/UsersManagement/UsersManagement";
+// import ApproveRiders from "../pages/Dashboard/ApproveRiders/ApproveRiders";
+// import UsersManagement from "../pages/Dashboard/UsersManagement/UsersManagement";
 import AdminRoute from "./AdminRoute";
-import AssignRiders from "../pages/Dashboard/AssignRiders/AssignRiders";
+// import AssignRiders from "../pages/Dashboard/AssignRiders/AssignRiders";
 import RiderRoute from "./RiderRoute";
-import AssignedDeliveries from "../pages/Dashboard/AssignDeliveries/AssignedDeliveries";
-import CompletedDeliveries from "../pages/Dashboard/CompletedDeliveries/CompletedDeliveries";
+// import AssignedDeliveries from "../pages/Dashboard/AssignDeliveries/AssignedDeliveries";
+// import CompletedDeliveries from "../pages/Dashboard/CompletedDeliveries/CompletedDeliveries";
 import AllProducts from "../pages/Products/AllProducts";
 import ProductDetails from "../pages/Products/ProductDetails";
 import BookingForm from "../pages/Booking/BookingForm";
@@ -34,11 +33,6 @@ export const router = createBrowserRouter([
       { path: "all-products", Component: AllProducts },
       { path: "product/:productId", Component: ProductDetails },
       { path: "booking/:id", Component: BookingForm },
-      // {
-      //   path: "/coverage",
-      //   Component: Coverage,
-      //   loader: () => fetch('/public/serviceCenters.json').then(res => res.json())
-      // },
     ]
   },
   {
@@ -54,21 +48,26 @@ export const router = createBrowserRouter([
     element: <PrivateRoute><DashboardLayout /></PrivateRoute>,
     children: [
       { path: "/dashboard/my-orders", Component: MyOrders },
-      // { path: "payment/:parcelId", Component: Payment },
+      { path: "payment/:parcelId", Component: Payment },
       { path: "/dashboard/track-order", Component: TrackOrder },
       { path: "/dashboard/profile", Component: Profile },
       { path: "payment-history", Component: PaymentHistory },
       { path: "payment-success", Component: PaymentSuccess },
       { path: "payment-cancelled", Component: PaymentCancelled },
 
+      {
+        path: "booking/:id",
+        element: <PrivateRoute><BookingForm></BookingForm></PrivateRoute>
+      },
+
       // rider-only
-      { path: "assigned-deliveries", element: <RiderRoute><AssignedDeliveries /></RiderRoute> },
-      { path: "completed-deliveries", element: <RiderRoute><CompletedDeliveries /></RiderRoute> },
+      // { path: "assigned-deliveries", element: <RiderRoute><AssignedDeliveries /></RiderRoute> },
+      // { path: "completed-deliveries", element: <RiderRoute><CompletedDeliveries /></RiderRoute> },
 
       // admin
-      { path: "approve-riders", element: <AdminRoute><ApproveRiders /></AdminRoute> },
-      { path: "assign-riders", element: <AdminRoute><AssignRiders /></AdminRoute> },
-      { path: "users-management", element: <AdminRoute><UsersManagement /></AdminRoute> },
+      // { path: "approve-riders", element: <AdminRoute><ApproveRiders /></AdminRoute> },
+      // { path: "assign-riders", element: <AdminRoute><AssignRiders /></AdminRoute> },
+      // { path: "users-management", element: <AdminRoute><UsersManagement /></AdminRoute> },
     ]
   }
 ]);
