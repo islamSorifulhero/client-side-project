0eaa214b89cad6d9fb18b1ad4ddc6f953fb39722
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-12-18 14:26:22 +0600

    https://server-side-one-eta.vercel.app/  added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://server-side-one-eta.vercel.app'
 })
 
 const useAxiosSecure = () => {
