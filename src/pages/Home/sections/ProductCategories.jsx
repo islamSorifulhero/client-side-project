d97517cd13d4283fa1f738f5e3fb5907340306b2
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-01-09 11:05:34 +0600

    video added

--- src/pages/Home/sections/ProductCategories.jsx
+++ src/pages/Home/sections/ProductCategories.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import Electronic from "../../../assets/Electronic-Devices.png"
 import Fashion from "../../../assets/Fashion.jpg"
 import HomeAppliances from "../../../assets/Home appliances.jpg"
+import garments from "../../../assets/garments.mp4"
 
 const categories = [
     { name: "Electronics", img: Electronic },
@@ -11,30 +12,35 @@ const categories = [
 
 const ProductCategories = () => {
     return (
-        <div className="max-w-6xl mx-auto px-4 py-12">
-            <h2 className="text-3xl font-bold mb-6 text-center">
-                Shop By Category
-            </h2>
+        <div>
+            <div>
+                <video autoPlay muted loop playsInline src={garments}></video>
+            </div>
+            <div className="max-w-6xl mx-auto px-4 py-12">
+                <h2 className="text-3xl font-bold mb-6 text-center">
+                    Shop By Category
+                </h2>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {categories.map((c, i) => (
-                    <motion.div
-                        key={i}
-                        whileHover={{ scale: 1.05 }}
-                        className="card shadow-xl bg-base-100 rounded-xl"
-                    >
-                        <figure>
-                            <img
-                                src={c.img}
-                                alt={c.name}
-                                className="h-48 w-full object-cover rounded-t-xl"
-                            />
-                        </figure>
-                        <div className="card-body text-center">
-                            <h2 className="text-xl font-semibold animate__animated animate__pulse animate__infinite">{c.name}</h2>
-                        </div>
-                    </motion.div>
-                ))}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {categories.map((c, i) => (
+                        <motion.div
+                            key={i}
+                            whileHover={{ scale: 1.05 }}
+                            className="card shadow-xl bg-base-100 rounded-xl"
+                        >
+                            <figure>
+                                <img
+                                    src={c.img}
+                                    alt={c.name}
+                                    className="h-48 w-full object-cover rounded-t-xl"
+                                />
+                            </figure>
+                            <div className="card-body text-center">
+                                <h2 className="text-xl font-semibold animate__animated animate__pulse animate__infinite">{c.name}</h2>
+                            </div>
+                        </motion.div>
+                    ))}
+                </div>
             </div>
         </div>
     );
